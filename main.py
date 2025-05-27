b2b2a5a5fe61d16ea41efaf6f9be69af6db483fd
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-27 19:11:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import discord
 from discord.ext import commands
-from bot.nct import register_nct
+#from bot.nct import register_nct
 from bot.scl import register_scl
 from bot.img import register_img
 from bot.girl import register_girl
 from bot.anime import register_anime
 from bot.emoji import register_emoji
 from bot.images import register_images
+from bot.cosplay import register_cosplay
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -20,13 +21,14 @@ async def on_ready():
     print(f'Bot {bot.user} đã đăng nhập thành công trên Discord!')
 
 # Đăng ký các lệnh/sự kiện từ các module (các hàm register_ cần được chuyển hướng theo discord.py)
-register_nct(bot)
+#register_nct(bot)
 register_scl(bot)
-register_scl(bot)
+register_img(bot)
 register_girl(bot)
 register_anime(bot)
 register_emoji(bot)
 register_images(bot)
+register_cosplay(bot)
 
 if __name__ == '__main__':
     print("Bot Discord đang hoạt động...")
