7fb43a3d5a97ae33389f880a7fa42c42e74f9b10
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-27 18:04:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import discord
 from discord.ext import commands
 from bot.nct import register_nct
 from bot.scl import register_scl
+from bot.img import register_img
 from bot.girl import register_girl
 from bot.anime import register_anime
 from bot.emoji import register_emoji
 from bot.images import register_images
 
-# Lấy token Discord từ biến môi trường (đặt tên biến là DISCORD_TOKEN)
 TOKEN = os.getenv("DISCORD_TOKEN")
-if not TOKEN:
-    raise ValueError("Không tìm thấy biến môi trường DISCORD_TOKEN!")
 
-# Khởi tạo intents và bot với prefix là "!"
 intents = discord.Intents.default()
 intents.message_content = True  # Cần bật nếu bạn muốn đọc nội dung tin nhắn
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -25,11 +22,12 @@ async def on_ready():
 # Đăng ký các lệnh/sự kiện từ các module (các hàm register_ cần được chuyển hướng theo discord.py)
 register_nct(bot)
 register_scl(bot)
+register_scl(bot)
 register_girl(bot)
 register_anime(bot)
 register_emoji(bot)
 register_images(bot)
 
 if __name__ == '__main__':
-    print("Khởi động bot Discord...")
+    print("Bot Discord đang hoạt động...")
     bot.run(TOKEN)
