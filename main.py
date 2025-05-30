1839ae19c3b37ce179149c1a321bec47a021ab78
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-30 18:52:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 import discord
 from discord.ext import commands
-#from bot.qr import register_qr
+from bot.qr import register_qr
 #from bot.nct import register_nct
 from bot.scl import register_scl
 from bot.img import register_img
@@ -24,7 +24,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True  # Cần bật nếu bạn muốn đọc nội dung tin nhắn
-bot = commands.Bot(command_prefix="!", intents=intents)
+#bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -37,7 +37,7 @@ async def on_ready():
         print(f"Lỗi khi đồng bộ lệnh slash: {e}")
 
 # Đăng ký các lệnh/sự kiện từ các module (các hàm register_ cần được chuyển hướng theo discord.py)
-#register_qr(bot)
+register_qr(bot)
 #register_nct(bot)
 register_scl(bot)
 register_img(bot)
