5c69c306766392fe3e813332d8663bfa58fb3b23
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-01 11:23:26 +0700

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
 
+WELCOME_CHANNEL_ID = 1375698214564528128
+GOODBYE_CHANNEL_ID = 1378583823041957899
+
 def register_notification(bot: commands.Bot):
-  @bot.event
-  async def on_member_join(member):
-      channel = discord.utils.get(member.guild.text_channels, name='Save all file')  # đổi tên kênh nếu khác
-      if channel:
-          await channel.send(f'👋 Chào mừng {member.mention} đến với server **{member.guild.name}**!')
-  
-  # Khi có thành viên rời server
-  @bot.event
-  async def on_member_remove(member):
-      channel = discord.utils.get(member.guild.text_channels, name='general')
-      if channel:
-          await channel.send(f'😢 {member.name} đã rời khỏi server.')
+    @bot.event
+    async def on_member_join(member):
+        channel = member.guild.get_channel(WELCOME_CHANNEL_ID)
+        if channel:
+            await channel.send(f'👋 Chào mừng {member.mention} đến với server **{member.guild.name}**!')
+
+    @bot.event
+    async def on_member_remove(member):
+        channel = member.guild.get_channel(GOODBYE_CHANNEL_ID)
+        if channel:
+            await channel.send(f'😢 {member.name} đã rời khỏi server.')
