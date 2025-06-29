1398087362d55823356ee5cf64cdaaba55fa5907
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-29 17:32:09 +0700

    Update scl.py

--- bot/scl.py
+++ bot/scl.py
@@ -193,8 +193,8 @@ def register_scl(bot):
         lines = ["**🎵 Kết quả tìm kiếm trên SoundCloud**\n"]
         for i, track in enumerate(tracks):
             lines.append(
-                f"{i + 1}. **{track['title']}**"
-                f"** » Nghệ sĩ:** {track['user']['username']}\n"
+                f"**{i + 1}. {track['title']}"
+                f" » Nghệ sĩ:** {track['user']['username']}\n"
                 f"** » Lượt nghe:** {track['playback_count']:,} | **Thích:** {track['likes_count']:,}\n"
             )
         lines.append("**💡 Chọn số bài hát bạn muốn tải!**")
